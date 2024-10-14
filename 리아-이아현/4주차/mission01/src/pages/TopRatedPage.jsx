a85954799cc7872e315da01498a49ebc7c1c3d36
[SW5] 이아현 <laah0602@gmail.com>
2024-10-15 01:43:46 +0900

    feat: 페이지 로딩 및 에러 처리

--- 리아-이아현/4주차/mission01/src/pages/TopRatedPage.jsx
+++ 리아-이아현/4주차/mission01/src/pages/TopRatedPage.jsx
@@ -8,6 +8,22 @@ const TopRatedPage = () => {
     isError,
   } = useCustomFetch(`/movie/top_rated?language=ko-KR&page=1`);
 
+  if (isLoading && movies.length === 0) {
+    return (
+      <div style={{ backgroundColor: "black" }}>
+        <h1 style={{ color: "white" }}>로딩 중 입니다...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>에러 입니다...</h1>
+      </div>
+    );
+  }
+
   return <MovieGrid movies={movies} />;
 };
 
--- 리아-이아현/4주차/mission01/src/pages/UpComingPage.jsx
+++ 리아-이아현/4주차/mission01/src/pages/UpComingPage.jsx
@@ -8,6 +8,22 @@ const UpComingPage = () => {
     isError,
   } = useCustomFetch(`/movie/upcoming?language=ko-KR&page=1`);
 
+  if (isLoading && movies.length === 0) {
+    return (
+      <div style={{ backgroundColor: "black" }}>
+        <h1 style={{ color: "white" }}>로딩 중 입니다...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>에러 입니다...</h1>
+      </div>
+    );
+  }
+
   return <MovieGrid movies={movies} />;
 };
 
