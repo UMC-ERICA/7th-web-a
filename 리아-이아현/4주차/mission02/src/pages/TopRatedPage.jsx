2e3bbfa0a04d479d27f6982f94ef7fac7267d5da
[SW5] 이아현 <laah0602@gmail.com>
2024-10-15 16:37:20 +0900

    setting: mission02 프로젝트 세팅

---
+++ 리아-이아현/4주차/mission02/src/pages/TopRatedPage.jsx
@@ -0,0 +1,30 @@
+import MovieGrid from "../components/MovieGrid";
+import useCustomFetch from "../hooks/useCustomFetch";
+
+const TopRatedPage = () => {
+  const {
+    data: movies,
+    isLoading,
+    isError,
+  } = useCustomFetch(`/movie/top_rated?language=ko-KR&page=1`);
+
+  if (isLoading && movies.length === 0) {
+    return (
+      <div style={{ backgroundColor: "black" }}>
+        <h1 style={{ color: "white" }}>로딩 중 입니다...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>에러 입니다...</h1>
+      </div>
+    );
+  }
+
+  return <MovieGrid movies={movies} />;
+};
+
+export default TopRatedPage;
