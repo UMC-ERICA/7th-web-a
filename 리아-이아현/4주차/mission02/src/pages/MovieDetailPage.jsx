9dc5a4c9471a6ac6fc34716de0d42b21de4e3134
[SW5] 이아현 <laah0602@gmail.com>
2024-10-16 13:33:10 +0900

    style: 상세페이지 영화 정보 스타일링

--- 리아-이아현/4주차/mission02/src/pages/MovieDetailPage.jsx
+++ 리아-이아현/4주차/mission02/src/pages/MovieDetailPage.jsx
@@ -1,11 +1,62 @@
 import { useParams } from "react-router-dom";
 import IMG_BASE_URL from "../constants/path";
 import useCustomFetchMovieDetail from "../hooks/useCustomFetchMovieDetail";
+import styled from "styled-components";
+
+const MovieDetailContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: black;
+  color: white;
+  padding: 50px;
+  height: 100vh;
+  width: 100%;
+  gap: 20px;
+`;
+
+const MoviePoster = styled.img`
+  width: 300px;
+  height: 350px;
+  border-radius: 15px;
+  object-fit: cover;
+  margin: 0 20px;
+`;
+
+const MovieInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 20px;
+`;
+
+const MovieTitle = styled.h1`
+  font-size: 30px;
+  margin-bottom: 20px;
+`;
+
+const MovieInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 15px;
+  margin-bottom: 5px;
+`;
+
+const MovieTagline = styled.div`
+  font-style: italic;
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: 30px;
+  margin-bottom: 10px;
+`;
+
+const MovieOverview = styled.div`
+  font-size: 20px;
+  margin-bottom: 10px;
+`;
 
 const MovieDetailPage = () => {
-  const { movieId } = useParams(); // movieId 추출
+  const { movieId } = useParams();
 
-  // 영화 상세 정보 데이터 가져오기
   const {
     data: movieDetails,
     isLoading,
@@ -28,23 +79,32 @@ const MovieDetailPage = () => {
     );
   }
 
-  // movieDetails가 없을 때 처리
   if (!movieDetails) {
     return <h1 style={{ color: "white" }}>영화 정보를 불러올 수 없습니다.</h1>;
   }
 
-  const { title, poster_path, release_date, overview, vote_average, runtime } =
-    movieDetails;
+  const {
+    title,
+    poster_path,
+    release_date,
+    overview,
+    vote_average,
+    runtime,
+    tagline,
+  } = movieDetails;
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <img src={`${IMG_BASE_URL}${poster_path}`} alt={title} />
-      <p>개봉일: {release_date}</p>
-      <p>평점: {vote_average}</p>
-      <p>상영 시간: {runtime}분</p>
-      <p>줄거리: {overview}</p>
-    </div>
+    <MovieDetailContainer>
+      <MoviePoster src={`${IMG_BASE_URL}${poster_path}`} alt={title} />
+      <MovieInfoContainer>
+        <MovieTitle>{title}</MovieTitle>
+        <MovieInfo>개봉일: {release_date}</MovieInfo>
+        <MovieInfo>평균: {vote_average}</MovieInfo>
+        <MovieInfo>상영 시간: {runtime}분</MovieInfo>
+        <MovieTagline>{tagline}</MovieTagline>
+        <MovieOverview>{overview}</MovieOverview>
+      </MovieInfoContainer>
+    </MovieDetailContainer>
   );
 };
 
