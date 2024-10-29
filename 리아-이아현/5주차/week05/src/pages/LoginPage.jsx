d7bf85db29903c2adc2aa0f1b346ece0eb8795fd
[SW5] 이아현 <laah0602@gmail.com>
2024-10-29 23:11:40 +0900

    style: 로그인 페이지 UI 구현

--- 리아-이아현/5주차/week05/src/pages/LoginPage.jsx
+++ 리아-이아현/5주차/week05/src/pages/LoginPage.jsx
@@ -1,19 +1,51 @@
 import styled from "styled-components";
 
 const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   background-color: #222;
   color: white;
-  height: 100vh;
   width: 100%;
-  font-weight: bold;
-  font-size: 25px;
-  padding: 30px 30px;
+  height: 60vh;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 150px 0;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 50px;
+`;
+
+const Input = styled.input`
+  width: 450px;
+  height: 30px;
+  padding: 10px;
+  margin: 10px 0;
+  font-size: 18px;
+  border: none;
+  border-radius: 8px;
+`;
+
+const LoginButton = styled.button`
+  width: 470px;
+  height: 60px;
+  padding: 10px;
+  font-size: 20px;
+  border: none;
+  border-radius: 8px;
+  background-color: #ff285e;
+  color: white;
+  cursor: pointer;
+  margin-top: 20px;
 `;
 
 const LoginPage = () => {
   return (
     <LoginContainer>
-      <h3>로그인 페이지</h3>
+      <Title>로그인</Title>
+      <Input type="email" placeholder="이메일을 입력해주세요!" />
+      <Input type="password" placeholder="비밀번호를 입력해주세요!" />
+      <LoginButton>로그인</LoginButton>
     </LoginContainer>
   );
 };
