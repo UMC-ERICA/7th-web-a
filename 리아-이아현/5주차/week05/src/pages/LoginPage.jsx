b7f79f0821e5013a3e04a5ad5dfab4f46619aef5
[SW5] 이아현 <laah0602@gmail.com>
2024-10-29 23:35:44 +0900

    feat: 이메일,비밀번호 형식 실시간 에러 검사 기능

--- 리아-이아현/5주차/week05/src/pages/LoginPage.jsx
+++ 리아-이아현/5주차/week05/src/pages/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const LoginContainer = styled.div`
@@ -26,6 +27,17 @@ const Input = styled.input`
   border-radius: 8px;
 `;
 
+const ErrorMessage = styled.p.attrs((props) => ({
+  isVisible: undefined,
+}))`
+  color: red;
+  font-size: 14px;
+  margin-top: -8px;
+  margin-bottom: 10px;
+  text-align: left;
+  visibility: ${({ $isVisible }) => ($isVisible ? "visible" : "hidden")};
+`;
+
 const LoginButton = styled.button`
   width: 470px;
   height: 60px;
@@ -40,11 +52,54 @@ const LoginButton = styled.button`
 `;
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+  };
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+    if (value.length < 8 || value.length > 16) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+  };
+
   return (
     <LoginContainer>
       <Title>로그인</Title>
-      <Input type="email" placeholder="이메일을 입력해주세요!" />
-      <Input type="password" placeholder="비밀번호를 입력해주세요!" />
+      <Input
+        type="email"
+        placeholder="이메일을 입력해주세요!"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <ErrorMessage $isVisible={emailError}>
+        올바른 이메일 형식이 아닙니다. 다시 확인해주세요!
+      </ErrorMessage>
+
+      <Input
+        type="password"
+        placeholder="비밀번호를 입력해주세요!"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <ErrorMessage $isVisible={passwordError}>
+        비밀번호는 8 ~ 16자리 사이로 입력해주세요!
+      </ErrorMessage>
+
       <LoginButton>로그인</LoginButton>
     </LoginContainer>
   );
