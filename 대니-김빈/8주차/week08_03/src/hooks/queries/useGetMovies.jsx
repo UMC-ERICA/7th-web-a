ee34fd279e001248a50d1e4ceb25b242d152713e
veen0219 <veen0219@naver.com>
2024-11-25 22:14:33 +0900

    미션 3 회원가입,로그인 페이지 티빙처럼 수정하기

---
+++ 대니-김빈/8주차/week08_03/src/hooks/queries/useGetMovies.jsx
@@ -0,0 +1,19 @@
+import axiosInstance from "../../apis/axios-instance";
+
+const useGetMovies = async ({ category, pageParam }) => {
+    const { data } = await axiosInstance.get(`movie/${category}?language=ko-KR&page=${pageParam}`);
+    
+    return data;
+};
+
+const fetchMovieData = async (movieId) => {
+    const { data } = await axiosInstance.get(`/movie/${movieId}?language=ko-KR`);
+    return data;
+};
+
+const fetchCreditsData = async (movieId) => {
+    const { data } = await axiosInstance.get(`/movie/${movieId}/credits?language=ko-KR`);
+    return data;
+};
+
+export { useGetMovies, fetchMovieData, fetchCreditsData };
